d72d0863f65476bec375113f741c431bf935c71f
Anıl <anil@Anl-MacBook-Air.local>
2023-12-08 19:52:14 +0300

    provider & context

--- src/components/TrackListItem.tsx
+++ src/components/TrackListItem.tsx
@@ -1,19 +1,23 @@
-import {View,Text,Image,StyleSheet} from "react-native";
+import {View,Text,Image,StyleSheet, Pressable} from "react-native";
 import { Track } from "../types";
+import { usePlayerContext } from "../providers/PlayerProvider";
 
 type TrackListItemProps = {
     track: Track
 }
 
 export default function TrackListItem({track}: TrackListItemProps) {
+
+    const {setTrack} = usePlayerContext();
+
     return (
-        <View style={styles.container}>
+        <Pressable onPress={() => setTrack(track)} style={styles.container}>
                 <Image  style={styles.image} source={{uri: track.album.images[0]?.url}} />
                 <View>
             <Text style={styles.title}>{track.name}</Text>
             <Text style={styles.subtitle}>{track.artists[0]?.name}</Text>
             </View>
-        </View>
+        </Pressable>
     )
 }
 
