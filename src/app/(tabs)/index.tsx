c0f851e3367e81398c48107078db2395e28a3b8d
Anıl <anil@Anl-MacBook-Air.local>
2023-12-08 19:22:43 +0300

    music player ui

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
-import { FlatList, StyleSheet,View,Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { tracks } from '../../../assets/data/tracks';
 import TrackListItem from '../../components/TrackListItem';
 
 export default function HomeScreen() {
   return (
-      <FlatList data={tracks} renderItem={({item}) => <TrackListItem track={item} />} showsVerticalScrollIndicator={false}/>
+    <FlatList
+      data={tracks}
+      renderItem={({ item }) => <TrackListItem track={item} />}
+      showsVerticalScrollIndicator={false}
+    />
   );
 }
-
-
-  
-
