d358b98241571bf662be59b9f716236d7406239e
Anıl <anil@Anl-MacBook-Air.local>
2023-12-13 20:14:44 +0300

    search query

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,13 +1,45 @@
-import { FlatList,View,TextInput,SafeAreaView,StyleSheet,Text} from 'react-native';
-import { tracks } from '../../../assets/data/tracks';
+import { FlatList,View,TextInput,SafeAreaView,StyleSheet,Text, ActivityIndicator} from 'react-native';
 import TrackListItem from '../../components/TrackListItem';
 import { FontAwesome } from '@expo/vector-icons';
 import { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
+
+const query = gql`
+query MyQuery {
+  search(q: "NF") {
+    tracks {
+      items {
+        id
+        name
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+          artists {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 export default function SearchScreen() {
-  
+
+
 const [search,setSearch]= useState("");
+
+const {data,loading,error} = useQuery(query ,{variables: {q: search}});
+
+const tracks = data?.search?.tracks?.items || [];
+
 
   return (
     <SafeAreaView>
@@ -20,6 +52,10 @@ const [search,setSearch]= useState("");
       placeholder='What do you want to listen to?'/>
       <Text onPress={() => setSearch("")} style={{color: "white"}}>Cancel</Text>
       </View>
+
+      {loading && <ActivityIndicator />}
+      {error && <Text>Failed to fetch tracks</Text>}
+      
      <FlatList 
      data={tracks} 
      renderItem={({item}) => <TrackListItem 
